560419bd2d42e9a3fd77389eefee1f59052f5770
Charvee Masand <charvee.masand@gmail.com>
2026-01-30 22:17:25 +0530

    Initial Commit

---
+++ Calm exam/Calm_examApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calm_examApp.swift
+//  Calm exam
+//
+//  Created by Charvee Masand on 30/01/26.
+//
+
+import SwiftUI
+
+@main
+struct Calm_examApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
